7ca9c3e2eeaab7553727f7b81b35e6079eba2c95
Daria Kravchuk <daria1kravchuk@gmail.com>
2024-04-05 12:30:07 +0300

    Add filter_lines method

--- main.py
+++ main.py
@@ -10,3 +10,18 @@ def read_file(file_path: str) -> list:
     with open(file_path, 'r') as file:
         lines = file.readlines()
     return lines
+
+
+def filter_lines(lines: list, keyword: str) -> list:
+    """
+    Функція для фільтрації рядків за певним ключовим словом.
+
+    :param lines: Список рядків для фільтрації
+    :type lines: list
+    :param keyword: Ключове слово для фільтрації
+    :type keyword: str
+    :return: Список відфільтрованих рядків
+    :rtype: list
+    """
+    filtered_lines = [line for line in lines if keyword in line]
+    return filtered_lines
