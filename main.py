22c5a096b9008f9439c2b1729d0df28d5ec08640
Daria Kravchuk <daria1kravchuk@gmail.com>
2024-04-05 12:32:52 +0300

    Add write_file method

--- main.py
+++ main.py
@@ -25,3 +25,19 @@ def filter_lines(lines: list, keyword: str) -> list:
     """
     filtered_lines = [line for line in lines if keyword in line]
     return filtered_lines
+
+
+def filter_file(input_file: str, output_file: str, keyword: str) -> None:
+    """
+    Головна функція для фільтрації файлу за певним ключовим словом.
+
+    :param input_file: Шлях до вхідного файлу
+    :type input_file: str
+    :param output_file: Шлях до вихідного файлу
+    :type output_file: str
+    :param keyword: Ключове слово для фільтрації
+    :type keyword: str
+    """
+    lines = read_file(input_file)
+    filtered_lines = filter_lines(lines, keyword)
+    write_file(output_file, filtered_lines)
